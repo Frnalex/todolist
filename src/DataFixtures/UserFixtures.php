7743d9c64a9a067d225f7d991f1798308a49df70
Alexandre Fournou <alexandre.fournou@gmail.com>
2022-03-29 21:07:46 +0200

    Attach task to user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Task;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -28,6 +29,17 @@ class UserFixtures extends Fixture
             ;
 
             $manager->persist($user);
+
+            for ($t = 1; $t <= rand(1, 4); ++$t) {
+                $task = new Task();
+
+                $task
+                    ->setTitle("task {$t}")
+                    ->setContent("Content task {$t}")
+                    ->setUser($user);
+
+                $manager->persist($task);
+            }
         }
 
         $manager->flush();
